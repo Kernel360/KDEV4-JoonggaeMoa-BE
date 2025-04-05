5cfd1298248d8d0b8a2aee4912451398d37b05b6
sohyeonjung <jhyeon23@konkuk.ac.kr>
2025-04-06 00:35:23 +0900

    [#74] feat: 알림창 용 markRead, getNotifications api 추가

---
+++ src/main/java/org/silsagusi/joonggaemoa/domain/notify/service/command/NotificationCommand.java
@@ -0,0 +1,22 @@
+package org.silsagusi.joonggaemoa.domain.notify.service.command;
+
+import lombok.Builder;
+import lombok.Getter;
+import org.silsagusi.joonggaemoa.domain.notify.entity.Notification;
+import org.silsagusi.joonggaemoa.domain.notify.entity.NotificationType;
+
+@Getter
+@Builder
+public class NotificationCommand {
+    private Long id;
+    private NotificationType type;
+    private String content;
+
+    public static NotificationCommand of(Notification notification) {
+        return NotificationCommand.builder()
+            .id(notification.getId())
+            .type(notification.getType())
+            .content(notification.getContent())
+            .build();
+    }
+}
