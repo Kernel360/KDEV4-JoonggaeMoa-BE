dd2bae9e3b5d229e423934ab7d9aa8304491b66d
sohyeonjung <jhyeon23@konkuk.ac.kr>
2025-04-05 23:39:32 +0900

    [#74] refactor: notification type을 enum으로 변경

--- src/main/java/org/silsagusi/joonggaemoa/domain/notify/entity/Notification.java
+++ src/main/java/org/silsagusi/joonggaemoa/domain/notify/entity/Notification.java
@@ -1,9 +1,6 @@
 package org.silsagusi.joonggaemoa.domain.notify.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.silsagusi.joonggaemoa.global.BaseEntity;
@@ -19,11 +16,12 @@ public class Notification extends BaseEntity {
 
     private Long agentId;
 
-    private String type;
+    @Enumerated(EnumType.STRING)
+    private NotificationType type;
     private String content;
     private boolean read = false;
 
-    public Notification(Long agentId, String type, String content) {
+    public Notification(Long agentId, NotificationType type, String content) {
         this.agentId = agentId;
         this.type = type;
         this.content = content;
