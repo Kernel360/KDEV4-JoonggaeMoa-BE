ba7497cfaa4458d86e5d36cbddb880e0517d0109
sohyeonjung <jhyeon23@konkuk.ac.kr>
2025-04-07 19:04:01 +0900

    [#84] feat: isRead 추가 및 request 방식 변경

--- src/main/java/org/silsagusi/joonggaemoa/domain/notify/service/command/NotificationCommand.java
+++ src/main/java/org/silsagusi/joonggaemoa/domain/notify/service/command/NotificationCommand.java
@@ -11,12 +11,14 @@ public class NotificationCommand {
     private Long id;
     private NotificationType type;
     private String content;
+    private Boolean isRead;
 
     public static NotificationCommand of(Notification notification) {
         return NotificationCommand.builder()
             .id(notification.getId())
             .type(notification.getType())
             .content(notification.getContent())
+            .isRead(notification.getIsRead())
             .build();
     }
 }
