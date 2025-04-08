172cba05cf7233444f451de045b502f35767d915
sohyeonjung <jhyeon23@konkuk.ac.kr>
2025-04-08 11:15:41 +0900

    [#86] refactor: 스케줄링 분리

--- src/main/java/org/silsagusi/joonggaemoa/global/config/ScheduleConfig.java
+++ src/main/java/org/silsagusi/joonggaemoa/global/config/ScheduleConfig.java
@@ -1,5 +1,7 @@
 package org.silsagusi.joonggaemoa.global.config;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.JobParameters;
 import org.springframework.batch.core.JobParametersBuilder;
 import org.springframework.batch.core.configuration.JobRegistry;
@@ -8,40 +10,50 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-
 @Slf4j
 @Configuration
 @EnableScheduling
 @RequiredArgsConstructor
 public class ScheduleConfig {
 
-	private final JobLauncher jobLauncher;
-	private final JobRegistry jobRegistry;
-	private static final String TIME_STAMP = "timeStamp";
-	private static final String TIME_ZONE = "Asia/Seoul";
+    private final JobLauncher jobLauncher;
+    private final JobRegistry jobRegistry;
+    private static final String TIME_STAMP = "timeStamp";
+    private static final String TIME_ZONE = "Asia/Seoul";
 
-	@Scheduled(cron = "0 0/30 * * * *", zone = TIME_ZONE)
-	public void scheduleNotificationsEvery30Min() throws Exception {
-		log.info("Sending messages & consultation notify schedule start");
+    @Scheduled(cron = "0 0/30 * * * *", zone = TIME_ZONE)
+    public void sendMessages() throws Exception {
+        log.info("Sending message schedule start");
 
-		JobParameters jobParameters = new JobParametersBuilder()
-			.addLong(TIME_STAMP, System.currentTimeMillis())
-			.toJobParameters();
+        JobParameters jobParameters = new JobParametersBuilder()
+            .addLong(TIME_STAMP, System.currentTimeMillis())
+            .toJobParameters();
 
-		jobLauncher.run(jobRegistry.getJob("messageSendingJob"), jobParameters);
-		jobLauncher.run(jobRegistry.getJob("todayConsultationNotifyJob"), jobParameters);
-	}
+        jobLauncher.run(jobRegistry.getJob("messageSendingJob"), jobParameters);
 
-	@Scheduled(cron = "0 0 * * * *", zone = TIME_ZONE)
-	public void notifyContractExpiry() throws Exception {
-		log.info("Checking contract expiration schedule start");
+    }
 
-		JobParameters jobParameters = new JobParametersBuilder()
-			.addLong(TIME_STAMP, System.currentTimeMillis())
-			.toJobParameters();
+    @Scheduled(cron = "0 0/30 * * * *", zone = TIME_ZONE)
+    public void notifyTodayConsultations() throws Exception {
+        log.info("Checking today's consultation schedule start");
 
-		jobLauncher.run(jobRegistry.getJob("contractExpireNotifyJob"), jobParameters);
-	}
+        JobParameters jobParameters = new JobParametersBuilder()
+            .addLong(TIME_STAMP, System.currentTimeMillis())
+            .toJobParameters();
+
+        jobLauncher.run(jobRegistry.getJob("todayConsultationNotifyJob"), jobParameters);
+
+    }
+
+    @Scheduled(cron = "0 0 * * * *", zone = TIME_ZONE)
+    public void notifyContractExpiry() throws Exception {
+        log.info("Checking contract expiration schedule start");
+
+        JobParameters jobParameters = new JobParametersBuilder()
+            .addString(TIME_STAMP, String.valueOf(System.currentTimeMillis()))
+            .toJobParameters();
+
+        jobLauncher.run(jobRegistry.getJob("contractExpireNotifyJob"), jobParameters);
+    }
+
 }
