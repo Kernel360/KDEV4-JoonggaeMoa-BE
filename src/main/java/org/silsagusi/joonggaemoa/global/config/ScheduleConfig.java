91a43a58633da9a04e2bb4fd6d3ef4cfb57b1fd3
sohyeonjung <jhyeon23@konkuk.ac.kr>
2025-04-08 11:27:12 +0900

    [#89] refactor: 스케줄러 비동기 처리

--- src/main/java/org/silsagusi/joonggaemoa/global/config/ScheduleConfig.java
+++ src/main/java/org/silsagusi/joonggaemoa/global/config/ScheduleConfig.java
@@ -6,13 +6,18 @@ import org.springframework.batch.core.JobParameters;
 import org.springframework.batch.core.JobParametersBuilder;
 import org.springframework.batch.core.configuration.JobRegistry;
 import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 
 @Slf4j
 @Configuration
 @EnableScheduling
+@EnableAsync
 @RequiredArgsConstructor
 public class ScheduleConfig {
 
@@ -21,6 +26,16 @@ public class ScheduleConfig {
     private static final String TIME_STAMP = "timeStamp";
     private static final String TIME_ZONE = "Asia/Seoul";
 
+    @Bean
+    public ThreadPoolTaskScheduler taskScheduler() {
+        ThreadPoolTaskScheduler scheduler = new ThreadPoolTaskScheduler();
+        scheduler.setPoolSize(5); // 작업 수
+        scheduler.setThreadNamePrefix("scheduler-thread");
+        scheduler.initialize();
+        return scheduler;
+    }
+
+    @Async
     @Scheduled(cron = "0 0/30 * * * *", zone = TIME_ZONE)
     public void sendMessages() throws Exception {
         log.info("Sending message schedule start");
@@ -33,6 +48,7 @@ public class ScheduleConfig {
 
     }
 
+    @Async
     @Scheduled(cron = "0 0/30 * * * *", zone = TIME_ZONE)
     public void notifyTodayConsultations() throws Exception {
         log.info("Checking today's consultation schedule start");
@@ -45,6 +61,7 @@ public class ScheduleConfig {
 
     }
 
+    @Async
     @Scheduled(cron = "0 0 * * * *", zone = TIME_ZONE)
     public void notifyContractExpiry() throws Exception {
         log.info("Checking contract expiration schedule start");
