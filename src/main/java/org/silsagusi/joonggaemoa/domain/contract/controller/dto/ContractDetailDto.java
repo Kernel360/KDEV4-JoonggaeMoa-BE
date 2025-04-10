da01713d43f4e2aec9b8647a745da909294db3a3
sohyeonjung <jhyeon23@konkuk.ac.kr>
2025-04-10 16:02:59 +0900

    [#104] fix: Contract id타입 string으로 변경

--- src/main/java/org/silsagusi/joonggaemoa/domain/contract/controller/dto/ContractDetailDto.java
+++ src/main/java/org/silsagusi/joonggaemoa/domain/contract/controller/dto/ContractDetailDto.java
@@ -1,45 +1,44 @@
 package org.silsagusi.joonggaemoa.domain.contract.controller.dto;
 
-import java.time.LocalDate;
-
-import org.silsagusi.joonggaemoa.domain.contract.service.command.ContractCommand;
-
 import lombok.Builder;
 import lombok.Getter;
+import org.silsagusi.joonggaemoa.domain.contract.service.command.ContractCommand;
+
+import java.time.LocalDate;
 
 public class ContractDetailDto {
 
-	@Getter
-	@Builder
-	public static class Response {
-		private Long id;
-		private Long landlordId;
-		private Long tenantId;
-		private String landlordName;
-		private String tenantName;
-		private String landlordPhone;
-		private String tenantPhone;
-		private String landlordEmail;
-		private String tenantEmail;
-		private LocalDate createdAt;
-		private LocalDate expiredAt;
-		private String url;
+    @Getter
+    @Builder
+    public static class Response {
+        private String id;
+        private Long landlordId;
+        private Long tenantId;
+        private String landlordName;
+        private String tenantName;
+        private String landlordPhone;
+        private String tenantPhone;
+        private String landlordEmail;
+        private String tenantEmail;
+        private LocalDate createdAt;
+        private LocalDate expiredAt;
+        private String url;
 
-		public static Response of(ContractCommand command) {
-			return Response.builder()
-				.id(command.getId())
-				.landlordId(command.getLandlordId())
-				.tenantId(command.getTenantId())
-				.landlordName(command.getLandlordName())
-				.tenantName(command.getTenantName())
-				.landlordPhone(command.getLandlordPhone())
-				.tenantPhone(command.getTenantPhone())
-				.landlordEmail(command.getLandlordEmail())
-				.tenantEmail(command.getTenantEmail())
-				.createdAt(command.getCreatedAt())
-				.expiredAt(command.getExpiredAt())
-				.url(command.getUrl())
-				.build();
-		}
-	}
+        public static Response of(ContractCommand command) {
+            return Response.builder()
+                .id(command.getId())
+                .landlordId(command.getLandlordId())
+                .tenantId(command.getTenantId())
+                .landlordName(command.getLandlordName())
+                .tenantName(command.getTenantName())
+                .landlordPhone(command.getLandlordPhone())
+                .tenantPhone(command.getTenantPhone())
+                .landlordEmail(command.getLandlordEmail())
+                .tenantEmail(command.getTenantEmail())
+                .createdAt(command.getCreatedAt())
+                .expiredAt(command.getExpiredAt())
+                .url(command.getUrl())
+                .build();
+        }
+    }
 }
--- src/main/java/org/silsagusi/joonggaemoa/domain/contract/service/command/ContractCommand.java
+++ src/main/java/org/silsagusi/joonggaemoa/domain/contract/service/command/ContractCommand.java
@@ -1,45 +1,44 @@
 package org.silsagusi.joonggaemoa.domain.contract.service.command;
 
-import java.time.LocalDate;
-
-import org.silsagusi.joonggaemoa.domain.contract.entity.Contract;
-
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
+import org.silsagusi.joonggaemoa.domain.contract.entity.Contract;
+
+import java.time.LocalDate;
 
 @Setter
 @Getter
 @Builder
 public class ContractCommand {
-	private Long id;
-	private Long landlordId;
-	private Long tenantId;
-	private String landlordName;
-	private String tenantName;
-	private String landlordPhone;
-	private String tenantPhone;
-	private String landlordEmail;
-	private String tenantEmail;
-	private LocalDate createdAt;
-	private LocalDate expiredAt;
-	private String url;
+    private String id;
+    private Long landlordId;
+    private Long tenantId;
+    private String landlordName;
+    private String tenantName;
+    private String landlordPhone;
+    private String tenantPhone;
+    private String landlordEmail;
+    private String tenantEmail;
+    private LocalDate createdAt;
+    private LocalDate expiredAt;
+    private String url;
 
-	public static ContractCommand of(Contract contract) {
-		return ContractCommand.builder()
-			.id(contract.getId())
-			.landlordId(contract.getCustomerLandlord().getId())
-			.tenantId(contract.getCustomerTenant().getId())
-			.landlordName(contract.getCustomerLandlord().getName())
-			.tenantName(contract.getCustomerTenant().getName())
-			.landlordPhone(contract.getCustomerLandlord().getPhone())
-			.tenantPhone(contract.getCustomerTenant().getPhone())
-			.landlordEmail(contract.getCustomerLandlord().getEmail())
-			.tenantEmail(contract.getCustomerTenant().getEmail())
-			.createdAt(contract.getCreatedAt())
-			.expiredAt(contract.getExpiredAt())
-			.url(contract.getUrl())
-			.build();
-	}
+    public static ContractCommand of(Contract contract) {
+        return ContractCommand.builder()
+            .id(contract.getId())
+            .landlordId(contract.getCustomerLandlord().getId())
+            .tenantId(contract.getCustomerTenant().getId())
+            .landlordName(contract.getCustomerLandlord().getName())
+            .tenantName(contract.getCustomerTenant().getName())
+            .landlordPhone(contract.getCustomerLandlord().getPhone())
+            .tenantPhone(contract.getCustomerTenant().getPhone())
+            .landlordEmail(contract.getCustomerLandlord().getEmail())
+            .tenantEmail(contract.getCustomerTenant().getEmail())
+            .createdAt(contract.getCreatedAt())
+            .expiredAt(contract.getExpiredAt())
+            .url(contract.getUrl())
+            .build();
+    }
 
 }
