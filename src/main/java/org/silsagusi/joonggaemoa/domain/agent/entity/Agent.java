ebba8c64f9bf85773984d83a2b806142456ffbf9
sohyeonjung <jhyeon23@konkuk.ac.kr>
2025-04-10 17:30:41 +0900

    [#109] feat: agent username, phone, email 중복 검증 로직 추가

--- src/main/java/org/silsagusi/joonggaemoa/domain/agent/entity/Agent.java
+++ src/main/java/org/silsagusi/joonggaemoa/domain/agent/entity/Agent.java
@@ -1,22 +1,13 @@
 package org.silsagusi.joonggaemoa.domain.agent.entity;
 
-import java.util.List;
-
-import org.hibernate.annotations.DynamicUpdate;
-import org.silsagusi.joonggaemoa.domain.customer.entity.Customer;
-
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
+import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.DynamicUpdate;
+import org.silsagusi.joonggaemoa.domain.customer.entity.Customer;
+
+import java.util.List;
 
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @DynamicUpdate
@@ -24,62 +15,59 @@ import lombok.NoArgsConstructor;
 @Getter
 public class Agent {
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Column(name = "agent_id")
-	private Long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "agent_id")
+    private Long id;
 
-	@OneToMany(fetch = FetchType.LAZY)
-	private List<Customer> customers;
+    @OneToMany(fetch = FetchType.LAZY)
+    private List<Customer> customers;
 
-	private String name;
+    private String name;
 
-	@Column(unique = true)
-	private String phone;
+    private String phone;
 
-	@Column(unique = true)
-	private String email;
+    private String email;
 
-	@Column(unique = true)
-	private String username;
+    private String username;
 
-	private String password;
+    private String password;
 
-	private String office;
+    private String office;
 
-	private String region;
+    private String region;
 
-	private String businessNo;
+    private String businessNo;
 
-	@Enumerated(EnumType.STRING)
-	private Role role;
+    @Enumerated(EnumType.STRING)
+    private Role role;
 
-	enum Role {
-		ROLE_AGENT, ROLE_CUSTOMER
-	}
+    enum Role {
+        ROLE_AGENT, ROLE_CUSTOMER
+    }
 
-	public Agent(String name, String phone, String email, String username, String password, String office,
-		String region, String businessNo) {
-		this.name = name;
-		this.phone = phone;
-		this.email = email;
-		this.username = username;
-		this.password = password;
-		this.office = office;
-		this.region = region;
-		this.businessNo = businessNo;
-		this.role = Role.ROLE_AGENT;
-	}
+    public Agent(String name, String phone, String email, String username, String password, String office,
+                 String region, String businessNo) {
+        this.name = name;
+        this.phone = phone;
+        this.email = email;
+        this.username = username;
+        this.password = password;
+        this.office = office;
+        this.region = region;
+        this.businessNo = businessNo;
+        this.role = Role.ROLE_AGENT;
+    }
 
-	public void updateAgent(String name, String phone, String email, String username, String office, String region,
-		String businessNo) {
-		this.name = name;
-		this.phone = phone;
-		this.email = email;
-		this.username = username;
-		this.office = office;
-		this.region = region;
-		this.businessNo = businessNo;
-	}
+    public void updateAgent(String name, String phone, String email, String username, String office, String region,
+                            String businessNo) {
+        this.name = name;
+        this.phone = phone;
+        this.email = email;
+        this.username = username;
+        this.office = office;
+        this.region = region;
+        this.businessNo = businessNo;
+    }
 
 }
--- src/main/java/org/silsagusi/joonggaemoa/domain/agent/service/AgentService.java
+++ src/main/java/org/silsagusi/joonggaemoa/domain/agent/service/AgentService.java
@@ -1,5 +1,7 @@
 package org.silsagusi.joonggaemoa.domain.agent.service;
 
+import io.jsonwebtoken.Claims;
+import lombok.RequiredArgsConstructor;
 import org.silsagusi.joonggaemoa.domain.agent.controller.dto.AgentUpdateRequest;
 import org.silsagusi.joonggaemoa.domain.agent.entity.Agent;
 import org.silsagusi.joonggaemoa.domain.agent.repository.AgentRepository;
@@ -13,79 +15,89 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import io.jsonwebtoken.Claims;
-import lombok.RequiredArgsConstructor;
-
 @Service
 @Transactional
 @RequiredArgsConstructor
 public class AgentService {
 
-	private final JwtProvider jwtProvider;
-	private final AgentRepository agentRepository;
-	private final BCryptPasswordEncoder bCryptPasswordEncoder;
-	private final RefreshTokenStore refreshTokenStore;
-	private final MessageTemplateService messageTemplateService;
+    private final JwtProvider jwtProvider;
+    private final AgentRepository agentRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+    private final RefreshTokenStore refreshTokenStore;
+    private final MessageTemplateService messageTemplateService;
 
-	@Transactional
-	public void signup(
-		String username,
-		String password,
-		String name,
-		String phone,
-		String email,
-		String office,
-		String region,
-		String businessNo
-	) {
+    @Transactional
+    public void signup(
+        String username,
+        String password,
+        String name,
+        String phone,
+        String email,
+        String office,
+        String region,
+        String businessNo
+    ) {
 
-		Agent agent = new Agent(
-			name,
-			phone,
-			email,
-			username,
-			bCryptPasswordEncoder.encode(password),
-			office,
-			region,
-			businessNo
-		);
+        if (agentRepository.existsByUsername(username)) {
+            throw new CustomException(ErrorCode.CONFLICT_USERNAME);
+        }
 
-		agentRepository.save(agent);
+        if (agentRepository.existsByPhone(phone)) {
+            throw new CustomException(ErrorCode.CONFLICT_PHONE);
+        }
 
-		messageTemplateService.createDefaultMessageTemplate(agent);
-	}
+        if (agentRepository.existsByEmail(email)) {
+            throw new CustomException(ErrorCode.CONFLICT_EMAIL);
+        }
 
-	public AgentCommand getAgentByNameAndPhone(String name, String phone) {
-		Agent agent = agentRepository.findByNameAndPhone(name, phone)
-			.orElseThrow(() -> new CustomException(ErrorCode.NOT_FOUND_USER));
 
-		return AgentCommand.of(agent);
-	}
+        Agent agent = new Agent(
+            name,
+            phone,
+            email,
+            username,
+            bCryptPasswordEncoder.encode(password),
+            office,
+            region,
+            businessNo
+        );
 
-	public void logout(String accessToken) {
-		if (Boolean.FALSE.equals(jwtProvider.validateToken(accessToken))) {
-			return;
-		}
-		Claims claims = jwtProvider.getClaims(accessToken);
-		String username = claims.get("username", String.class);
+        agentRepository.save(agent);
 
-		refreshTokenStore.deleteRefreshToken(username);
-	}
+        messageTemplateService.createDefaultMessageTemplate(agent);
+    }
 
-	public AgentCommand getAgent(Long agentId) {
-		Agent agent = agentRepository.findById(agentId)
-			.orElseThrow(() -> new CustomException(ErrorCode.NOT_FOUND_USER));
+    public AgentCommand getAgentByNameAndPhone(String name, String phone) {
+        Agent agent = agentRepository.findByNameAndPhone(name, phone)
+            .orElseThrow(() -> new CustomException(ErrorCode.NOT_FOUND_USER));
 
-		return AgentCommand.of(agent);
-	}
+        return AgentCommand.of(agent);
+    }
 
-	public void updateAgent(Long agentId, AgentUpdateRequest requestDto) {
-		Agent agent = agentRepository.findById(agentId)
-			.orElseThrow(() -> new CustomException(ErrorCode.NOT_FOUND_USER));
+    public void logout(String accessToken) {
+        if (Boolean.FALSE.equals(jwtProvider.validateToken(accessToken))) {
+            return;
+        }
+        Claims claims = jwtProvider.getClaims(accessToken);
+        String username = claims.get("username", String.class);
 
-		agent.updateAgent(requestDto.getName(), requestDto.getPhone(), requestDto.getEmail(), requestDto.getUsername(),
-			requestDto.getOffice(), requestDto.getRegion(), requestDto.getBusinessNo());
+        refreshTokenStore.deleteRefreshToken(username);
+    }
 
-		agentRepository.save(agent);
-	}
+    public AgentCommand getAgent(Long agentId) {
+        Agent agent = agentRepository.findById(agentId)
+            .orElseThrow(() -> new CustomException(ErrorCode.NOT_FOUND_USER));
+
+        return AgentCommand.of(agent);
+    }
+
+    public void updateAgent(Long agentId, AgentUpdateRequest requestDto) {
+        Agent agent = agentRepository.findById(agentId)
+            .orElseThrow(() -> new CustomException(ErrorCode.NOT_FOUND_USER));
+
+        agent.updateAgent(requestDto.getName(), requestDto.getPhone(), requestDto.getEmail(), requestDto.getUsername(),
+            requestDto.getOffice(), requestDto.getRegion(), requestDto.getBusinessNo());
+
+        agentRepository.save(agent);
+    }
 }
