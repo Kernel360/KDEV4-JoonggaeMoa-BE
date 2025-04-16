2eecc7ab2757b16a6e993f6e6bd8a2518820afc2
Seungmok1 <kww4215@naver.com>
2025-04-16 21:11:22 +0900

    [#145] refactor: custom response 수정

--- core/src/main/java/org/silsagusi/core/customResponse/ApiResponse.java
+++ core/src/main/java/org/silsagusi/core/customResponse/ApiResponse.java
@@ -3,8 +3,8 @@ package org.silsagusi.core.customResponse;
 import org.silsagusi.core.customResponse.exception.CustomException;
 import org.silsagusi.core.customResponse.exception.ExceptionDto;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 
 import jakarta.annotation.Nullable;
@@ -16,27 +16,24 @@ import lombok.Getter;
 @AllArgsConstructor
 public class ApiResponse<T> {
 
-	@JsonIgnore
-	HttpStatus httpStatus;
 	boolean success;
 	@Nullable
 	T data;
 	@Nullable
 	ExceptionDto error;
 
+	@ResponseStatus(HttpStatus.OK)
 	public static <T> ApiResponse<T> ok() {
-		return new ApiResponse<>(HttpStatus.OK, true, null, null);
+		return new ApiResponse<>(true, null, null);
 	}
 
+	@ResponseStatus(HttpStatus.OK)
 	public static <T> ApiResponse<T> ok(final T data) {
-		return new ApiResponse<>(HttpStatus.OK, true, data, null);
+		return new ApiResponse<>(true, data, null);
 	}
 
-	public static <T> ApiResponse<T> created(@Nullable final T data) {
-		return new ApiResponse<>(HttpStatus.CREATED, true, data, null);
-	}
-
+	@ResponseStatus(HttpStatus.BAD_REQUEST)
 	public static <T> ApiResponse<T> fail(final CustomException e) {
-		return new ApiResponse<>(HttpStatus.BAD_REQUEST, false, null, ExceptionDto.of(e.getErrorCode()));
+		return new ApiResponse<>(false, null, ExceptionDto.of(e.getErrorCode()));
 	}
 }
