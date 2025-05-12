25679a65a8da5614a256c207eafe7ecd450c74a0
허용석 <johnsonheo96@gmail.com>
2025-05-12 17:47:09 +0900

    [#288] REFACT: build.gradle 최적화

--- core/src/test/java/org/silsagusi/core/CoreApplicationTests.java
+++ core/src/test/java/org/silsagusi/core/CoreApplicationTests.java
@@ -1,9 +1,7 @@
 package org.silsagusi.core;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class CoreApplicationTests {
 
 	@Test
