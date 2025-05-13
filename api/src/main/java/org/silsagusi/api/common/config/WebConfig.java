92e656c97a7b14995a7d8335ac6eb1c0ef990237
Seungmok1 <kww4215@naver.com>
2025-05-13 15:24:33 +0900

    [#297] fix: 페이지네이션 응답값 수정

--- api/src/main/java/org/silsagusi/api/common/config/WebConfig.java
+++ api/src/main/java/org/silsagusi/api/common/config/WebConfig.java
@@ -1,17 +1,13 @@
 package org.silsagusi.api.common.config;
 
+import java.util.List;
+
 import org.silsagusi.api.common.util.CurrentAgentIdResolver;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.web.config.EnableSpringDataWebSupport;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import java.util.List;
-
 @Configuration
-@EnableSpringDataWebSupport(
-	pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO
-)
 public class WebConfig implements WebMvcConfigurer {
 
 	@Override
